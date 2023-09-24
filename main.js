4f495fd5110fd84474de5478cb403c5cab0916da
SSH <12si47bun@gmail.com>
2023-09-25 08:03:33 +0900

    공격함수 일반화, UI추가

--- main.js
+++ main.js
@@ -10,6 +10,84 @@ let damageRange = 1;
 
 /// 함수정의 단락
 
+// 공격방향 정의
+const directionsThunder = [
+  { dx: 1, dy: 1 },  
+  { dx: 1, dy: -1 }, 
+  { dx: -1, dy: -1 }, 
+  { dx: -1, dy: 1 }   
+];
+const directionsFlame = [
+  { dx: 0, dy: 1 },  
+  { dx: 0, dy: -1 }, 
+  { dx: -1, dy: 0 }, 
+  { dx: 1, dy: 0 }   
+];
+const directionsAqua = [
+  { dx: 1, dy: 1 },  
+  { dx: 1, dy: -1 }, 
+  { dx: -1, dy: -1 }, 
+  { dx: -1, dy: 1 },
+  { dx: 0, dy: 1 },  
+  { dx: 0, dy: -1 }, 
+  { dx: -1, dy: 0 }, 
+  { dx: 1, dy: 0 }  
+];
+
+// 공격함수 (일반화)
+function attackType(cell, directions, damageMultiplication) {
+  // 클릭한 셀의 좌표
+  const [xCoordinate, yCoordinate] = cell.id.split("y").map(coord => parseInt(coord.replace(/\D/g, '')));
+
+  // 상하좌우 셀에 데미지 적용
+  const cellDamageGroup = [];
+  
+  // 각 방향에 대한 셀을 검사하고 유효한 경우에만 추가
+  for (const direction of directions) {
+    const x = xCoordinate + direction.dx * damageRange;
+    const y = yCoordinate + direction.dy * damageRange;
+    const attackCell = document.getElementById("x" + x + "y" + y);
+    if (attackCell) {
+      cellDamageGroup.push(attackCell);
+    }
+  }
+
+  console.log('셀 그룹:', cellDamageGroup); 
+  
+  // damageManager 함수를 호출하여 데미지 적용
+  damageManager(cellDamageGroup, damageMultiplication);
+};
+
+// 데미지 적용 함수
+function damageManager(cellDamageGroup, damageMultiplication ){for (const adjacentCell of cellDamageGroup) {
+  const adjacentElement = document.getElementById(adjacentCell.id);
+  if (adjacentElement) {
+    const currentValue = parseInt(adjacentElement.textContent);
+    const newValue = currentValue - damage * damageMultiplication;
+    // 0미만으로 hp감소 X
+    adjacentElement.textContent = Math.max(0, newValue).toString();
+    
+    // adjacentElement의 배경색 설정
+    const value = parseInt(adjacentElement.textContent);
+    adjacentElement.classList.remove("hp-0", "hp-1", "hp-2", "hp-3", "hp-4", "hp-5");
+
+    if (value === 0) {
+      adjacentElement.classList.add("hp-0");
+    } else if (value === 1) {
+      adjacentElement.classList.add("hp-1");
+    } else if (value === 2) {
+      adjacentElement.classList.add("hp-2");
+    } else if (value === 3) {
+      adjacentElement.classList.add("hp-3");
+    } else if (value === 4) {
+      adjacentElement.classList.add("hp-4");
+    } else if (value === 5) {
+      adjacentElement.classList.add("hp-5");
+    } 
+  }
+}};
+
+
 // 필드 초기화 함수
 function initializeField() {
   // id가 "field"인 요소를 찾습니다.
@@ -40,12 +118,13 @@ function initializeField() {
   row.appendChild(cell);
 }
 
-
   // 셀 클릭 이벤트 핸들러 함수
   function handleCellClick(event) {
     const cell = event.currentTarget;
     console.log("선택된 셀의 좌표 :", cell.id);
-    applyDamage(cell);
+    // attackType(cell, directionsThunder, 3);
+    attackType(cell, directionsFlame, 2);
+    // attackType(cell, directionsAqua, 1);
   }
 
   // 필드 테이블 생성 함수 호출
@@ -54,51 +133,6 @@ function initializeField() {
   // 테이블을 필드에 추가
   fieldElement.appendChild(tableElement);
 };
-
-// 셀 주변의 데미지 적용 함수
-function applyDamage(cell) {
-  const cellDamageGroup = [];
-  const [xCoordinate, yCoordinate] = cell.id.split("y").map(coord => parseInt(coord.replace(/\D/g, '')));
-
-  for (let x = xCoordinate - damageRange; x <= xCoordinate + damageRange; x++) {
-    for (let y = yCoordinate - damageRange; y <= yCoordinate + damageRange; y++) {
-      const adjacentCell = document.getElementById("x" + x + "y" + y);
-      if (adjacentCell) {
-        cellDamageGroup.push(adjacentCell);
-      }
-    }
-  }
-
-  console.log('셀 그룹:', cellDamageGroup);
-
-  for (const adjacentCell of cellDamageGroup) {
-    const adjacentElement = document.getElementById(adjacentCell.id);
-    if (adjacentElement) {
-      const currentValue = parseInt(adjacentElement.textContent);
-      const newValue = currentValue - damage;
-      adjacentElement.textContent = Math.max(0, newValue).toString();
-      
-      // adjacentElement의 배경색 설정
-      const value = parseInt(adjacentElement.textContent);
-      adjacentElement.classList.remove("hp-0", "hp-1", "hp-2", "hp-3", "hp-4", "hp-5");
-
-      if (value === 0) {
-        adjacentElement.classList.add("hp-0");
-      } else if (value === 1) {
-        adjacentElement.classList.add("hp-1");
-      } else if (value === 2) {
-        adjacentElement.classList.add("hp-2");
-      } else if (value === 3) {
-        adjacentElement.classList.add("hp-3");
-      } else if (value === 4) {
-        adjacentElement.classList.add("hp-4");
-      } else if (value === 5) {
-        adjacentElement.classList.add("hp-5");
-      } 
-    }
-  }
-}
-
 
 
 // --------------------------------------------------------------------------------------
